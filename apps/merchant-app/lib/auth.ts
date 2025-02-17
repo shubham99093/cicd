5afec76a9787fb36540923f1ef444fffdf0c7118
SHUBHAM CHODVADIYA <shubhamchodavadiya1122@gmail.com>
2025-02-17 18:16:07 +0530

    fix:moduleNotFound

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -1,48 +1,73 @@
-import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
-    callbacks: {
-      async signIn({ user, account }: {
-        user: {
-          email: string;
-          name: string
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        phone: {
+          label: "Phone number",
+          type: "text",
+          placeholder: "1231231231",
+          required: true,
         },
-        account: {
-          provider: "google" | "github"
-        }
-      }) {
-        console.log("hi signin")
-        if (!user || !user.email) {
-          return false;
-        }
-
-        await db.merchant.upsert({
-          select: {
-            id: true
-          },
+        password: { label: "Password", type: "password", required: true },
+      },
+      // TODO: User credentials type from next-aut
+      async authorize(credentials: any) {
+        // Do zod validation, OTP validation here
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+        const existingUser = await db.user.findFirst({
           where: {
-            email: user.email
+            number: credentials.phone,
           },
-          create: {
-            email: user.email,
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          },
-          update: {
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          }
         });
 
-        return true;
-      }
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.number,
+            };
+          }
+          return null;
+        }
+
+        try {
+          const user = await db.user.create({
+            data: {
+              number: credentials.phone,
+              password: hashedPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.number,
+          };
+        } catch (e) {
+          console.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    // TODO: can u fix the type here? Using any is bad
+    async session({ token, session }: any) {
+      session.user.id = token.sub;
+
+      return session;
     },
-    secret: process.env.NEXTAUTH_SECRET || "secret"
-  }
+  },
+};
