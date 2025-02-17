2339486285f3a62c720ce212b7a37c82c7420ad8
SHUBHAM CHODVADIYA <shubhamchodavadiya1122@gmail.com>
2025-02-17 12:09:04 +0530

    setup

---
+++ apps/user-app/components/SendCard.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Center } from "@repo/ui/center";
+import { TextInput } from "@repo/ui/textinput";
+import { useState } from "react";
+import { p2pTransfer } from "../app/lib/actions/p2pTransfer";
+
+export function SendCard() {
+  const [number, setNumber] = useState("");
+  const [amount, setAmount] = useState("");
+  const [res, setRes] = useState({ message: "" });
+
+  return (
+    <div className="h-[90vh]">
+      <Center>
+        <Card title="Send">
+          <div className="min-w-72 pt-2">
+            <TextInput
+              placeholder={"Number"}
+              label="Number"
+              onChange={(value) => {
+                setNumber(value);
+              }}
+            />
+            <TextInput
+              placeholder={"Amount"}
+              label="Amount"
+              onChange={(value) => {
+                setAmount(value);
+              }}
+            />
+            {res.message && (
+              <div
+                className={`pt-4 text-center ${res.message === "Success" ? "text-green-600" : "text-red-600"}`}
+              >
+                {res.message}
+              </div>
+            )}
+            <div className="pt-4 flex justify-center">
+              <Button
+                onClick={async () => {
+                  const res = await p2pTransfer(number, Number(amount) * 100);
+                  setRes(res);
+                }}
+              >
+                Send
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </Center>
+    </div>
+  );
+}
